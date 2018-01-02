910e762e0cdfa97965c996795fc157ae665fac45
Illya Yanchuk <illya.yanchuk@gmail.com>
2018-01-01 18:01:04 -0800

    corrected main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var http = require("http");
 http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
 
-  response.end('Hello World!\n';)
+  response.end('Hello World!\n');
 }).listen(3000);
 
 console.log('Server running on port 3000...')
